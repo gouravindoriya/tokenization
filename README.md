cbeb24a31631f4a49af6500559b1b9a3b75787b9
gouravindoriya <gouravindoriya00@gmail.com>
2025-08-13 00:26:18 +0530

    update

--- README.md
+++ README.md
@@ -1,70 +1,59 @@
-# React + TypeScript + Vite
+# Tokenization
+Tokenization is the process of breaking down a piece of text into smaller units called tokens. These tokens are often words, punctuation marks, or even parts of words.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Currently, two official plugins are available:
+![alt text](image.png)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-## Expanding the ESLint configuration
+### demo link   https://tokenization-assignment.netlify.app/
 
-If you are developing a production application, we recommend updating the configuration to enable type-aware lint rules:
+## Installation
 
-```js
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      // Other configs...
+```bash
+# Clone the repo
+git clone https://github.com/gouravindoriya/tokenization.git
 
-      // Remove tseslint.configs.recommended and replace with this
-      ...tseslint.configs.recommendedTypeChecked,
-      // Alternatively, use this for stricter rules
-      ...tseslint.configs.strictTypeChecked,
-      // Optionally, add this for stylistic rules
-      ...tseslint.configs.stylisticTypeChecked,
+# Go into the project folder
+cd tokonization
 
-      // Other configs...
-    ],
-    languageOptions: {
-      parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
-      // other options...
-    },
-  },
-])
+# Install dependencies
+npm install
+
+# Run the development server
+npm run dev
 ```
 
-You can also install [eslint-plugin-react-x](https://github.com/Rel1cx/eslint-react/tree/main/packages/plugins/eslint-plugin-react-x) and [eslint-plugin-react-dom](https://github.com/Rel1cx/eslint-react/tree/main/packages/plugins/eslint-plugin-react-dom) for React-specific lint rules:
 
 ```js
-// eslint.config.js
-import reactX from 'eslint-plugin-react-x'
-import reactDom from 'eslint-plugin-react-dom'
+# customEncode
+ function customEncode(text: string) {
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      // Other configs...
-      // Enable lint rules for React
-      reactX.configs['recommended-typescript'],
-      // Enable lint rules for React DOM
-      reactDom.configs.recommended,
-    ],
-    languageOptions: {
-      parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
-      // other options...
-    },
-  },
-])
+    const tokens = text.match(regex) || [];
+    const ids = tokens.map(token => {
+      if (!(token in vocab)) {
+        vocab[token] = idCounter++;
+      }
+      return vocab[token];
+    });
+
+    return { tokens, ids };
+  }
+
 ```
-# tokenization
+
+
+
+
+
+
+
+## Tech Stack
+- **Frontend:** React + Tailwind CSS + typescript
+- **Tokenization:** custom implemention with regex
+- **Deployment:** Netlify
+
+## Features
+- Displays token count and estimated cost for LLM APIs.
+- Real-time tokenization preview as you type.
+- Copy tokens to clipboard.
+- Dark & Light mode UI.
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css'
 
 function App() {
 
   const vocab: Record<string, number> = {};
-
-
   let idCounter = 1;
-
+  const ref=useRef(null)
+  const [regex,setregex]=useState(/@\w+|#\w+|[\u0900-\u097F]+|\p{Extended_Pictographic}|\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b|\w+(?:'\w+)?|[.,!?;]|\s+/gu)
   function customEncode(text: string) {
-    const regex = /@\w+|#\w+|[\u0900-\u097F]+|\p{Extended_Pictographic}|\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b|\w+(?:'\w+)?|[.,!?;]|\s+/gu
-
 
     const tokens = text.match(regex) || [];
-
     const ids = tokens.map(token => {
       if (!(token in vocab)) {
         vocab[token] = idCounter++;
@@ -28,59 +24,66 @@ function App() {
   }
 
 
-function customDecode(ids: number[]) {
-  // Reverse vocab: ID -> token
-  const vocabtype_2: Record<number, string> = {};
-  for (const token in vocab) {
-    const id = vocab[token];
-    vocabtype_2[id] = token; // number as key
-  }
-
-  console.log("Reversed vocab:", vocabtype_2);
-
-  let str = "";
-
-  for (const id of ids) {
-    const token = vocabtype_2[id];
-    if (token !== undefined) {
-      str += token;
-    } else {
-      str += "[UNK]"; // unknown token placeholder
+  function customDecode(ids: number[]) {
+    // Reverse vocab: ID -> token
+    const vocabtype_2: Record<number, string> = {};
+    for (const token in vocab) {
+      const id = vocab[token];
+      vocabtype_2[id] = token; // number as key
     }
+
+    console.log("Reversed vocab:", vocabtype_2);
+
+    let str = "";
+
+    for (const id of ids) {
+      const token = vocabtype_2[id];
+      if (token !== undefined) {
+        str += token;
+      }
+    }
+
+    console.log("Decoded string:", str);
+    return str;
   }
-
-  console.log("Decoded string:", str);
-  return str;
-}
-
-
-
-
 
 
   const [tokenData, settokenData] = useState(customEncode(""));
-
-  // console.log(tokenData)
-  // console.log(customDecode(tokenData.ids))
 
 
 
   return (
     <>
-      <div className="grid grid-cols-4 bg-gray-100 min-h-screen gap-3 p-4">
+      <div className="col-span-4 flex justify-center items-center text-4xl font-bold bg-white shadow-md rounded-lg py-4">
+        {/* HTML, CSS & JS */}
+        Tokenization
+      </div>
+      <div className="grid grid-cols-4 bg-gray-100 min-h-[30vh] gap-3 p-4">
 
         {/* Header */}
-        <div className="col-span-4 flex justify-center items-center text-4xl font-bold bg-white shadow-md rounded-lg py-4">
-          {/* HTML, CSS & JS */}
-          Tokenization
-        </div>
+
 
         {/* Textarea Section */}
         <div className="col-span-4 bg-white p-4 rounded-lg shadow-md flex flex-col gap-3">
+          <div className=' grid gap-2'>
+
+        
+          <label className="text-base font-medium text-gray-800 ">Custom Regex</label>
+          <input ref={ref} type="text " className='w-full min-h-[30px] p-4 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 outline-none resize-none'
+          placeholder={`default:  ${regex} - working soon `}
+          onChange={(e)=>setregex(e.target.value||regex)}
+          />
+          <button className=" inline-flex w-fit items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200">
+<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-transparent ">
+Cyan to blue
+</span>
+</button>
+
+            </div>
           <label className="text-base font-medium text-gray-800">Your message</label>
           <textarea
             className="w-full min-h-[150px] p-4 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 
-                     focus:ring-2 focus:ring-blue-400 focus:border-blue-400 outline-none resize-none"
+                     outline-none resize-none"
             placeholder="Write your thoughts here..."
 
             onChange={(e) => {
@@ -88,32 +91,31 @@ function customDecode(ids: number[]) {
             }}
           ></textarea>
 
-          <div className='bg-white p-4 rounded-lg shadow-md text-sm font-mono text-gray-700 flex flex-wrap gap-1'>
+       
+          {/* <div className='bg-white p-4 rounded-lg shadow-md text-sm font-mono text-gray-700 flex flex-wrap gap-1'>
+            {
+              customDecode(tokenData.ids)
+            }
+          </div> */}
+        </div>
+         
+
+        
+
+
+
+      </div>
+
+        <div className='w-screen bg-white p-4  text-sm font-mono text-gray-700 flex flex-wrap gap-2 items-center'>
             {
               tokenData.tokens.map((token, index) => {
                 if (token == " ") return;
-                return <div key={index} className='inline p-1 bg-green-200 h-fit'>{token}</div>
+                return <div key={index} className='inline p-4 py-2 h-fit shadow-sm bg-indigo-50  backdrop-blur-2xl rounded'>{token}</div>
               }
 
               )
             }
           </div>
-          <div className='bg-white p-4 rounded-lg shadow-md text-sm font-mono text-gray-700 flex flex-wrap gap-1'>
-            {
-             customDecode(tokenData.ids)
-            }
-          </div>
-        </div>
-
-        {/* IDs Section */}
-        <div className="col-span-2 bg-white p-4 rounded-lg shadow-md  text-sm font-mono text-gray-700 break-words">
-          {tokenData.ids.toString()}
-
-        </div>
-
-
-
-      </div>
     </>
   )
 }
